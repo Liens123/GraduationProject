54b986f254a31af6e449015189f5df0adf59c219
Liens123 <liens6666a@gmail.com>
2025-04-05 13:59:48 +0800

    数据分析系统第二次提交

--- src/main/java/org/example/graduation_project/model/AdminUser.java
+++ src/main/java/org/example/graduation_project/model/AdminUser.java
@@ -1,5 +1,85 @@
 package org.example.graduation_project.model;
 
-public class AdminUser {
+import java.util.Date;
 
+public class AdminUser {
+    private Long id;
+
+    private String username;
+
+    private String name;
+
+    private String password;
+
+    private Integer status;
+
+    private String operator;
+
+    private Date createdTime;
+
+    private Date updatedTime;
+
+    public Date getCreatedTime() {
+        return createdTime;
+    }
+
+    public void setCreatedTime(Date createdTime) {
+        this.createdTime = createdTime;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getOperator() {
+        return operator;
+    }
+
+    public void setOperator(String operator) {
+        this.operator = operator;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    public Date getUpdatedTime() {
+        return updatedTime;
+    }
+
+    public void setUpdatedTime(Date updatedTime) {
+        this.updatedTime = updatedTime;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
 }
